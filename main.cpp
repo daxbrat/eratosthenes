7b439ebe910ae4691a77143c8640bb93ecc58016
Ricio Dax Brathwaite <dax.dacosta@gmail.com>
2023-08-26 18:51:23 -0600

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,28 @@
+#include <iostream>
+#include <vector>
+
+using namespace std;
+
+int main() {
+    const int limit = 100;
+    vector<bool> isPrime(limit + 1, true);
+
+    isPrime[0] = isPrime[1] = false;
+
+    for (int i = 2; i * i <= limit; ++i) {
+        if (isPrime[i]) {
+            for (int j = i * i; j <= limit; j += i) {
+                isPrime[j] = false;
+            }
+        }
+    }
+
+    std::cout << "Prime numbers between 1 and " << limit << " are:\n";
+    for (int i = 2; i <= limit; ++i) {
+        if (isPrime[i]) {
+            cout << i << " ";
+        }
+    }
+
+    return 0;
+}
